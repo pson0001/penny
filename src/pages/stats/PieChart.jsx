f02c309a5d0fc9b186625ac6815816671c283dce
Ping Song <pson0001@hotmail.com>
2024-05-27 21:11:33 +1000

    add chart

---
+++ src/pages/stats/PieChart.jsx
@@ -0,0 +1,85 @@
+import { Pie } from "@ant-design/plots";
+import { ResponsivePie } from "@nivo/pie";
+
+const PieChart = ({ data }) => {
+  return (
+    <ResponsivePie
+      data={data}
+      theme={{
+        tooltip: {
+          wrapper: {},
+          container: {
+            background: "#101010",
+            color: "#fff",
+            fontSize: 14,
+          },
+        },
+      }}
+      margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+      innerRadius={0.5}
+      padAngle={0.7}
+      cornerRadius={3}
+      activeOuterRadiusOffset={8}
+      borderWidth={1}
+      borderColor={{
+        from: "color",
+        modifiers: [["darker", 0.5]],
+      }}
+      arcLinkLabelsSkipAngle={10}
+      arcLinkLabelsTextColor="#fff"
+      arcLinkLabelsThickness={2}
+      arcLinkLabelsColor={{ from: "color" }}
+      arcLabelsSkipAngle={10}
+      arcLabelsTextColor={{
+        from: "color",
+        modifiers: [["darker", 2]],
+      }}
+      defs={[
+        {
+          id: "dots",
+          type: "patternDots",
+          background: "inherit",
+          color: "rgba(255, 255, 255, 0.3)",
+          size: 4,
+          padding: 1,
+          stagger: true,
+        },
+        {
+          id: "lines",
+          type: "patternLines",
+          background: "inherit",
+          color: "rgba(255, 255, 255, 0.3)",
+          rotation: -45,
+          lineWidth: 6,
+          spacing: 10,
+        },
+      ]}
+      legends={[
+        {
+          anchor: "bottom",
+          direction: "row",
+          justify: false,
+          translateX: 0,
+          translateY: 56,
+          itemsSpacing: 0,
+          itemWidth: 100,
+          itemHeight: 18,
+          itemTextColor: "#fff",
+          itemDirection: "left-to-right",
+          itemOpacity: 1,
+          symbolSize: 18,
+          symbolShape: "circle",
+          effects: [
+            {
+              on: "hover",
+              style: {
+                itemTextColor: "#fff",
+              },
+            },
+          ],
+        },
+      ]}
+    />
+  );
+};
+export default PieChart;
--- src/pages/stats/Stats.jsx
+++ src/pages/stats/Stats.jsx
@@ -1,6 +1,63 @@
+import { useState } from "react";
 import PageContainer from "../../components/pageContainer/PageContainer";
-
+import PieChart from "./PieChart";
+import RealTimeData from "../../components/data/realTimeData";
+import moment from "moment";
+import { Flex, Radio } from "antd";
+import c from "./stats.module.scss";
 const Stats = () => {
-  return <PageContainer>Stats</PageContainer>;
+  const [chartType, setChartType] = useState("pie");
+  const [duration, setDuration] = useState(7);
+
+  const durationOptions = [7, 30, 365];
+  const chartOptions = ["Pie"];
+
+  const onChange = (e) => {
+    setDuration(e.target.value);
+  };
+  let allExpenses = [];
+  RealTimeData().forEach((element) => {
+    if (
+      element.transaction_date.isAfter(new moment().subtract(duration, "d")) &
+      element.transaction_date.isBefore(new moment()) &
+      (element.transaction_type === "out")
+    ) {
+      allExpenses.push(element);
+    }
+  });
+
+  const categorySums = allExpenses?.reduce((acc, transaction) => {
+    const category = transaction.transaction_category;
+    const amount = parseFloat(transaction.transaction_amount);
+
+    if (!acc[category]) {
+      acc[category] = 0;
+    }
+
+    acc[category] += parseInt(amount);
+    return acc;
+  }, {});
+
+  const result = Object.entries(categorySums).map(([id, value]) => ({
+    id,
+    value,
+  }));
+
+  return (
+    <PageContainer>
+      <div className={c.optionsContainer}>
+        <Flex vertical gap="middle">
+          <Radio.Group onChange={onChange} defaultValue={7}>
+            {durationOptions.map((option) => (
+              <Radio.Button value={option}>{option}</Radio.Button>
+            ))}
+          </Radio.Group>
+        </Flex>
+      </div>
+      <div className={c.chartContainer}>
+        {result.length > 0 && <PieChart data={result} />}
+      </div>
+    </PageContainer>
+  );
 };
 export default Stats;
